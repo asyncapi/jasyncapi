2f49d080b6b8e26d0e48742277f571951ff0523d
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2020-05-03 16:49:39 +0300

    [bindings]: @EqualsAndHashCode to super classes.

--- src/main/java/com/asyncapi/v2/binding/MessageBinding.java
+++ src/main/java/com/asyncapi/v2/binding/MessageBinding.java
@@ -1,9 +1,12 @@
 package com.asyncapi.v2.binding;
 
+import lombok.EqualsAndHashCode;
+
 /**
  * This class defines how to describe AsyncAPI message binding.
  *
  * @author Pavel Bodiachevskii
  */
+@EqualsAndHashCode
 public class MessageBinding {
 }
--- src/main/java/com/asyncapi/v2/binding/OperationBinding.java
+++ src/main/java/com/asyncapi/v2/binding/OperationBinding.java
@@ -1,9 +1,12 @@
 package com.asyncapi.v2.binding;
 
+import lombok.EqualsAndHashCode;
+
 /**
  * This class defines how to describe AsyncAPI operation binding.
  *
  * @author Pavel Bodiachevskii
  */
+@EqualsAndHashCode
 public class OperationBinding {
 }
