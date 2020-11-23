c874507a393f88dbe56b2a248e337479ee0b1c33
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2020-11-23 17:04:52 +0300

    asyncapi-idea: initial commit.

---
+++ asyncapi-plugin/asyncapi-plugin-idea/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.6.4"
+    java
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.asyncapi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2020.2.3"
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
