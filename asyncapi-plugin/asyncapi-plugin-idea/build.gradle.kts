18fc275f7669159067b1072d80458563fdb3724a
Daniel Kocot <danielkocot@icloud.com>
2021-07-30 10:54:39 +0200

    changed testCompile to testImplementation

--- asyncapi-plugin/asyncapi-plugin-idea/build.gradle.kts
+++ asyncapi-plugin/asyncapi-plugin-idea/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
