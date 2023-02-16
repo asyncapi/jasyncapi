f20c15bdf952b3d4c37734cbd5afa7505fb4f2f4
Guillaume Lamirand <guillaume.lamirand@graviteesource.com>
2023-02-16 17:21:25 +0100

    fix: additionalProperties should be a Boolean or a Schema

---
+++ asyncapi-core/src/main/java/com/asyncapi/v2/jackson/SchemasAdditionalPropertiesDeserializer.java
@@ -0,0 +1,34 @@
+package com.asyncapi.v2.jackson;
+
+import com.asyncapi.v2._0_0.model.schema.Schema;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+/**
+ * @author Guillaume LAMIRAND (guillaume.lamirand at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class SchemasAdditionalPropertiesDeserializer extends JsonDeserializer<Object> {
+
+    @Override
+    public Object deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        ObjectCodec objectCodec = p.getCodec();
+        JsonNode node = objectCodec.readTree(p);
+
+        return chooseKnownPojo(node, objectCodec);
+    }
+
+    private Object chooseKnownPojo(JsonNode jsonNode, final ObjectCodec objectCodec) throws IOException {
+        if (jsonNode.isBoolean()) {
+            return jsonNode.asBoolean();
+        } else {
+            return jsonNode.traverse(objectCodec).readValueAs(Schema.class);
+        }
+    }
+}
