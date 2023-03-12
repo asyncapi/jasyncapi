b06a24e9b42d9cfeb1ef578bdc1f789037d9828b
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-12 16:02:33 +0400

    chore: 2.0.0 new tests
    - ExternalDocumentation
    - Reference
    - Tag

--- asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/ExternalDocumentationTest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/ExternalDocumentationTest.kt
@@ -1,33 +1,24 @@
 package com.asyncapi.v2._0_0.model
 
-import com.asyncapi.v2.ClasspathUtils
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import com.asyncapi.v2.SerDeTest
 
 /**
  * @author Pavel Bodiachevskii
  */
-class ExternalDocumentationTest {
+class ExternalDocumentationTest: SerDeTest<ExternalDocumentation>() {
 
-    private val objectMapper = ObjectMapper()
+    override fun objectClass() = ExternalDocumentation::class.java
 
-    private fun buildExternalDocumentation(): ExternalDocumentation {
+    override fun baseObjectJson() = "/json/2.0.0/model/externalDocumentation.json"
+
+    override fun extendedObjectJson() = "/json/2.0.0/model/externalDocumentation - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/externalDocumentation - wrongly extended.json"
+
+    override fun build(): ExternalDocumentation {
         return ExternalDocumentation(
                 "Find more info here",
                 "https://example.com"
-        )
-    }
-
-    @Test
-    @DisplayName("Compare hand crafted model with parsed json")
-    fun compareModelWithParsedJson() {
-        val model = ClasspathUtils.readAsString("/json/2.0.0/model/externalDocumentation.json")
-
-        Assertions.assertEquals(
-                objectMapper.readValue(model, ExternalDocumentation::class.java),
-                buildExternalDocumentation()
         )
     }
 
--- asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/TagTest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/TagTest.kt
@@ -1,34 +1,25 @@
 package com.asyncapi.v2._0_0.model
 
-import com.asyncapi.v2.ClasspathUtils
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import com.asyncapi.v2.SerDeTest
 
 /**
  * @author Pavel Bodiachevskii
  */
-class TagTest {
+class TagTest: SerDeTest<Tag>() {
 
-    private val objectMapper = ObjectMapper()
+    override fun objectClass() = Tag::class.java
 
-    private fun buildTag(): Tag {
+    override fun baseObjectJson() = "/json/2.0.0/model/tag.json"
+
+    override fun extendedObjectJson() = "/json/2.0.0/model/tag - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/tag - wrongly extended.json"
+
+    override fun build(): Tag {
         return Tag(
                 "user",
                 "User-related messages",
-                null
-        )
-    }
-
-    @Test
-    @DisplayName("Compare hand crafted model with parsed json")
-    fun compareModelWithParsedJson() {
-        val model = ClasspathUtils.readAsString("/json/2.0.0/model/tag.json")
-
-        Assertions.assertEquals(
-                objectMapper.readValue(model, Tag::class.java),
-                buildTag()
+                ExternalDocumentationTest().build()
         )
     }
 
