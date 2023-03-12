495b12da39fd1f0b6a9cbafc682f2a4981a17aa6
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-12 16:08:41 +0400

    chore: 2.0.0 ServerVariable new tests

--- asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/server/ServerVariableTest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/server/ServerVariableTest.kt
@@ -1,6 +1,8 @@
 package com.asyncapi.v2._0_0.model.server
 
 import com.asyncapi.v2.ClasspathUtils
+import com.asyncapi.v2.SerDeTest
+import com.asyncapi.v2._6_0.model.server.ServerVariable
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
@@ -9,26 +11,23 @@ import org.junit.jupiter.api.Test
 /**
  * @author Pavel Bodiachevskii
  */
-class ServerVariableTest {
+class ServerVariableTest: SerDeTest<ServerVariable>() {
 
-    private val objectMapper = ObjectMapper()
+    override fun objectClass() = ServerVariable::class.java
 
-    private fun buildServerVariable(): ServerVariable {
+    override fun baseObjectJson() = "/json/2.0.0/model/server/serverVariable.json"
+
+    override fun extendedObjectJson() = "/json/2.0.0/model/server/serverVariable - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/server/serverVariable - wrongly extended.json"
+
+    override fun build(): ServerVariable {
         return ServerVariable.builder()
                 .enumValues(listOf("8883", "8884"))
+                .description("To which port connect")
                 .defaultValue("8883")
+                .examples(listOf("8883", "8884"))
                 .build()
-    }
-
-    @Test
-    @DisplayName("Compare hand crafted model with parsed json")
-    fun compareModelWithParsedJson() {
-        val model = ClasspathUtils.readAsString("/json/2.0.0/model/server/serverVariable.json")
-
-        Assertions.assertEquals(
-                objectMapper.readValue(model, ServerVariable::class.java),
-                buildServerVariable()
-        )
     }
 
 }
