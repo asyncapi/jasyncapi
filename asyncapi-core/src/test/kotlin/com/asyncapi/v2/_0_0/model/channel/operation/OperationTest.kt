dffdba26cc1e344ee60dd4379fceb1fd75ac8b61
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-13 00:31:47 +0400

    chore: 2.0.0 new tests
    - Operation
    - OperationTrait

--- asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/channel/operation/OperationTest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/channel/operation/OperationTest.kt
@@ -1,80 +1,126 @@
 package com.asyncapi.v2._0_0.model.channel.operation
 
-import com.asyncapi.v2.ClasspathUtils
+import com.asyncapi.v2.SerDeTest
+import com.asyncapi.v2._0_0.model.ExternalDocumentation
 import com.asyncapi.v2._0_0.model.Reference
 import com.asyncapi.v2._0_0.model.Tag
-import com.asyncapi.v2._0_0.model.channel.message.Message
-import com.asyncapi.v2._0_0.model.schema.Schema
-import com.asyncapi.v2._0_0.model.schema.Type
-import com.asyncapi.v2.binding.operation.amqp.AMQPOperationBinding
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import com.asyncapi.v2._0_0.model.channel.message.MessageTest
+import com.asyncapi.v2.binding.operation.OperationBinding
+import com.asyncapi.v2.binding.operation.amqp.AMQPOperationBindingTest
+import com.asyncapi.v2.binding.operation.amqp1.AMQP1OperationBinding
+import com.asyncapi.v2.binding.operation.anypointmq.AnypointMQOperationBinding
+import com.asyncapi.v2.binding.operation.googlepubsub.GooglePubSubOperationBinding
+import com.asyncapi.v2.binding.operation.http.HTTPOperationBindingTest
+import com.asyncapi.v2.binding.operation.ibmmq.IBMMQOperationBinding
+import com.asyncapi.v2.binding.operation.jms.JMSOperationBinding
+import com.asyncapi.v2.binding.operation.kafka.KafkaOperationBindingTest
+import com.asyncapi.v2.binding.operation.mercure.MercureOperationBinding
+import com.asyncapi.v2.binding.operation.mqtt.MQTTOperationBindingTest
+import com.asyncapi.v2.binding.operation.mqtt5.MQTT5OperationBinding
+import com.asyncapi.v2.binding.operation.nats.NATSOperationBindingTest
+import com.asyncapi.v2.binding.operation.pulsar.PulsarOperationBinding
+import com.asyncapi.v2.binding.operation.redis.RedisOperationBinding
+import com.asyncapi.v2.binding.operation.sns.SNSOperationBinding
+import com.asyncapi.v2.binding.operation.solace.SolaceOperationBindingTest
+import com.asyncapi.v2.binding.operation.sqs.SQSOperationBinding
+import com.asyncapi.v2.binding.operation.stomp.STOMPOperationBinding
+import com.asyncapi.v2.binding.operation.ws.WebSocketsOperationBinding
 
 /**
  * @author Pavel Bodiachevskii
  */
-class OperationTest {
+class OperationWithReferenceToMessageTest: SerDeTest<Operation>() {
 
-    private val objectMapper = ObjectMapper().registerKotlinModule()
+    override fun objectClass() = Operation::class.java
 
-    private fun buildOperation(): Operation {
+    override fun baseObjectJson() = "/json/2.0.0/model/channel/operation/operation with reference to message.json"
+
+    override fun extendedObjectJson() = "/json/2.0.0/model/channel/operation/operation with reference to message - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/channel/operation/operation with reference to message - wrongly extended.json"
+
+    override fun build(): Operation {
         return Operation.builder()
-                .operationId("registerUser")
-                .summary("Action to sign a user up.")
-                .description("A longer description")
+                .operationId("sendMessage")
+                .summary("Send message")
+                .description("Send message to remote server")
                 .tags(listOf(
-                        Tag("user", null, null),
-                        Tag("signup", null, null),
-                        Tag("register", null, null)
-                ))
-                .message(Message.builder()
-                        .headers(Schema.builder()
-                                .type(Type.OBJECT)
-                                .properties(mapOf(
-                                        Pair("applicationInstanceId", Schema.builder()
-                                                .type(Type.STRING)
-                                                .description("Unique identifier for a given instance of the publishing application")
-                                                .build()
-                                        )
-                                ))
+                        Tag.builder()
+                                .name("messages")
+                                .description("operations with messages")
+                                .externalDocs(ExternalDocumentation("Messages validation rules", "messages/validation-rules"))
                                 .build()
-                        )
-                        .payload(Schema.builder()
-                                .type(Type.OBJECT)
-                                .properties(mapOf(
-                                        Pair("user", Schema.builder().ref("#/components/schemas/userCreate").build()),
-                                        Pair("signup", Schema.builder().ref("#/components/schemas/signup").build())
-                                ))
-                                .build()
-                        )
-                        .build()
-                )
-                .bindings(mutableMapOf(
-                        Pair("amqp", AMQPOperationBinding.builder().ack(false).build())
                 ))
+                .externalDocs(ExternalDocumentation("Messages sending rules", "messages/sending-rules"))
+                .bindings(OperationTest.bindings())
                 .traits(listOf(
-                        Reference("#/components/operationTraits/kafka"),
-                        OperationTrait.builder()
-                                .bindings(mapOf(
-                                        Pair("amqp", AMQPOperationBinding.builder().ack(false).build())
-                                ))
-                                .build()
+                        Reference("#/components/operationTraits/sendMessage"),
+                        OperationTraitTest().build()
                 ))
+                .message(Reference("#/components/schemas/sendMessage"))
                 .build()
     }
 
-    @Test
-    @DisplayName("Compare hand crafted model with parsed json")
-    fun compareModelWithParsedJson() {
-        val model = ClasspathUtils.readAsString("/json/2.0.0/model/channel/operation/operation.json")
+}
 
-        Assertions.assertEquals(
-                objectMapper.readValue(model, Operation::class.java),
-                buildOperation()
-        )
+class OperationWithMessageTest: SerDeTest<Operation>() {
+
+    override fun objectClass() = Operation::class.java
+
+    override fun baseObjectJson() = "/json/2.0.0/model/channel/operation/operation with message.json"
+
+    override fun extendedObjectJson() = "/json/2.0.0/model/channel/operation/operation with message - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/channel/operation/operation with message - wrongly extended.json"
+
+    override fun build(): Operation {
+        return Operation.builder()
+                .operationId("sendMessage")
+                .summary("Send message")
+                .description("Send message to remote server")
+                .tags(listOf(
+                        Tag.builder()
+                                .name("messages")
+                                .description("operations with messages")
+                                .externalDocs(ExternalDocumentation("Messages validation rules", "messages/validation-rules"))
+                                .build()
+                ))
+                .externalDocs(ExternalDocumentation("Messages sending rules", "messages/sending-rules"))
+                .bindings(OperationTest.bindings())
+                .traits(listOf(
+                        Reference("#/components/operationTraits/sendMessage"),
+                        OperationTraitTest().build()
+                ))
+                .message(MessageTest().build())
+                .build()
     }
+}
 
+class OperationTest {
+    companion object {
+        @JvmStatic
+        fun bindings(): Map<String, OperationBinding> {
+            return mapOf(
+                    Pair("amqp", AMQPOperationBindingTest().build()),
+                    Pair("amqp1", AMQP1OperationBinding()),
+                    Pair("anypointmq", AnypointMQOperationBinding()),
+                    Pair("googlepubsub", GooglePubSubOperationBinding()),
+                    Pair("http", HTTPOperationBindingTest().build()),
+                    Pair("ibmmq", IBMMQOperationBinding()),
+                    Pair("jms", JMSOperationBinding()),
+                    Pair("kafka", KafkaOperationBindingTest().build()),
+                    Pair("mercure", MercureOperationBinding()),
+                    Pair("mqtt", MQTTOperationBindingTest().build()),
+                    Pair("mqtt5", MQTT5OperationBinding()),
+                    Pair("nats", NATSOperationBindingTest().build()),
+                    Pair("pulsar", PulsarOperationBinding()),
+                    Pair("redis", RedisOperationBinding()),
+                    Pair("sns", SNSOperationBinding()),
+                    Pair("solace", SolaceOperationBindingTest().build()),
+                    Pair("sqs", SQSOperationBinding()),
+                    Pair("stomp", STOMPOperationBinding()),
+                    Pair("ws", WebSocketsOperationBinding())
+            )
+        }
+    }
 }
--- asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/channel/operation/OperationTraitTest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/channel/operation/OperationTraitTest.kt
@@ -1,38 +1,76 @@
 package com.asyncapi.v2._0_0.model.channel.operation
 
-import com.asyncapi.v2.ClasspathUtils
-import com.asyncapi.v2.binding.operation.amqp.AMQPOperationBinding
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import com.asyncapi.v2.SerDeTest
+import com.asyncapi.v2._0_0.model.ExternalDocumentation
+import com.asyncapi.v2._0_0.model.Tag
+import com.asyncapi.v2.binding.operation.amqp.AMQPOperationBindingTest
+import com.asyncapi.v2.binding.operation.amqp1.AMQP1OperationBinding
+import com.asyncapi.v2.binding.operation.anypointmq.AnypointMQOperationBinding
+import com.asyncapi.v2.binding.operation.googlepubsub.GooglePubSubOperationBinding
+import com.asyncapi.v2.binding.operation.http.HTTPOperationBindingTest
+import com.asyncapi.v2.binding.operation.ibmmq.IBMMQOperationBinding
+import com.asyncapi.v2.binding.operation.jms.JMSOperationBinding
+import com.asyncapi.v2.binding.operation.kafka.KafkaOperationBindingTest
+import com.asyncapi.v2.binding.operation.mercure.MercureOperationBinding
+import com.asyncapi.v2.binding.operation.mqtt.MQTTOperationBindingTest
+import com.asyncapi.v2.binding.operation.mqtt5.MQTT5OperationBinding
+import com.asyncapi.v2.binding.operation.nats.NATSOperationBindingTest
+import com.asyncapi.v2.binding.operation.pulsar.PulsarOperationBinding
+import com.asyncapi.v2.binding.operation.redis.RedisOperationBinding
+import com.asyncapi.v2.binding.operation.sns.SNSOperationBinding
+import com.asyncapi.v2.binding.operation.solace.SolaceOperationBindingTest
+import com.asyncapi.v2.binding.operation.sqs.SQSOperationBinding
+import com.asyncapi.v2.binding.operation.stomp.STOMPOperationBinding
+import com.asyncapi.v2.binding.operation.ws.WebSocketsOperationBinding
 
 /**
  * @author Pavel Bodiachevskii
  */
-class OperationTraitTest {
+class OperationTraitTest: SerDeTest<OperationTrait>() {
 
-    private val objectMapper = ObjectMapper()
+    override fun objectClass() = OperationTrait::class.java
 
-    private fun buildOperationTrait(): OperationTrait {
+    override fun baseObjectJson() = "/json/2.0.0/model/channel/operation/operationTrait.json"
+
+    override fun extendedObjectJson() = "/json/2.0.0/model/channel/operation/operationTrait - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/channel/operation/operationTrait - wrongly extended.json"
+
+    override fun build(): OperationTrait {
         return OperationTrait.builder()
-                .bindings(mutableMapOf(
-                        Pair("amqp", AMQPOperationBinding.builder()
-                                .ack(false).build()
-                        )
+                .operationId("sendMessage")
+                .summary("Send message")
+                .description("Send message to remote server")
+                .tags(listOf(
+                        Tag.builder()
+                                .name("messages")
+                                .description("operations with messages")
+                                .externalDocs(ExternalDocumentation("Messages validation rules", "messages/validation-rules"))
+                                .build()
+                ))
+                .externalDocs(ExternalDocumentation("Messages sending rules", "messages/sending-rules"))
+                .bindings(mapOf(
+                        Pair("amqp", AMQPOperationBindingTest().build()),
+                        Pair("amqp1", AMQP1OperationBinding()),
+                        Pair("anypointmq", AnypointMQOperationBinding()),
+                        Pair("googlepubsub", GooglePubSubOperationBinding()),
+                        Pair("http", HTTPOperationBindingTest().build()),
+                        Pair("ibmmq", IBMMQOperationBinding()),
+                        Pair("jms", JMSOperationBinding()),
+                        Pair("kafka", KafkaOperationBindingTest().build()),
+                        Pair("mercure", MercureOperationBinding()),
+                        Pair("mqtt", MQTTOperationBindingTest().build()),
+                        Pair("mqtt5", MQTT5OperationBinding()),
+                        Pair("nats", NATSOperationBindingTest().build()),
+                        Pair("pulsar", PulsarOperationBinding()),
+                        Pair("redis", RedisOperationBinding()),
+                        Pair("sns", SNSOperationBinding()),
+                        Pair("solace", SolaceOperationBindingTest().build()),
+                        Pair("sqs", SQSOperationBinding()),
+                        Pair("stomp", STOMPOperationBinding()),
+                        Pair("ws", WebSocketsOperationBinding())
                 ))
                 .build()
-    }
-
-    @Test
-    @DisplayName("Compare hand crafted model with parsed json")
-    fun compareModelWithParsedJson() {
-        val model = ClasspathUtils.readAsString("/json/2.0.0/model/channel/operation/operationTrait.json")
-
-        Assertions.assertEquals(
-                objectMapper.readValue(model, OperationTrait::class.java),
-                buildOperationTrait()
-        )
     }
 
 }
