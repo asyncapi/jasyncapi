855f1e6b21864f1d5f2f0b3580011a97c98c2a13
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-13 00:41:35 +0400

    chore: 2.0.0 Parameter new tests

--- asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/channel/ParameterTest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/channel/ParameterTest.kt
@@ -1,37 +1,27 @@
 package com.asyncapi.v2._0_0.model.channel
 
-import com.asyncapi.v2.ClasspathUtils
+import com.asyncapi.v2.SerDeTest
 import com.asyncapi.v2._0_0.model.schema.Schema
-import com.asyncapi.v2._0_0.model.schema.Type
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 
 /**
  * @author Pavel Bodiachevskii
  */
-class ParameterTest {
+class ParameterTest: SerDeTest<Parameter>() {
 
-    private val objectMapper = ObjectMapper()
+    override fun objectClass() = Parameter::class.java
 
-    private fun buildParameter(): Parameter {
-        return Parameter(
-                "Id of the user.",
-                Schema.builder().type(Type.STRING).build(),
-                "\$message.payload#/user/id"
-        )
-    }
+    override fun baseObjectJson() = "/json/2.0.0/model/channel/parameter.json"
 
-    @Test
-    @DisplayName("Compare hand crafted model with parsed json")
-    fun compareModelWithParsedJson() {
-        val model = ClasspathUtils.readAsString("/json/2.0.0/model/channel/parameter.json")
+    override fun extendedObjectJson() = "/json/2.0.0/model/channel/parameter - extended.json"
 
-        Assertions.assertEquals(
-                objectMapper.readValue(model, Parameter::class.java),
-                buildParameter()
-        )
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/channel/parameter - wrongly extended.json"
+
+    override fun build(): Parameter {
+        return Parameter.builder()
+                .description("Id of the user.")
+                .location("\$message.payload#/user/id")
+                .schema(Schema.builder().type("string").build())
+                .build()
     }
 
 }
