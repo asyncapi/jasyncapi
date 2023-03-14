4a33226a9263e1483f438d1e714b5a48dd4e9e62
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-14 23:24:04 +0400

    chore: 2.0.0 AsyncAPI new tests

--- asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/AsyncAPITest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v2/_0_0/model/AsyncAPITest.kt
@@ -1,95 +1,41 @@
 package com.asyncapi.v2._0_0.model
 
-import com.asyncapi.v2.ClasspathUtils
-import com.asyncapi.v2._0_0.model.channel.ChannelItem
-import com.asyncapi.v2._0_0.model.component.Components
-import com.asyncapi.v2._0_0.model.info.Info
-import com.asyncapi.v2._0_0.model.server.Server
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Disabled
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import com.asyncapi.v2.SerDeTest
+import com.asyncapi.v2._0_0.model.channel.ChannelItemTest
+import com.asyncapi.v2._0_0.model.component.ComponentsTest
+import com.asyncapi.v2._0_0.model.info.InfoTest
+import com.asyncapi.v2._0_0.model.server.ServerTest
 
 /**
  * @author Pavel Bodiachevskii
  */
-@Disabled
-class AsyncAPITest {
+class AsyncAPITest: SerDeTest<AsyncAPI>() {
 
-    private val objectMapper = ObjectMapper()
+    override fun objectClass() = AsyncAPI::class.java
 
-    private fun buildInfo(): Info {
-        return objectMapper.readValue(
-                ClasspathUtils.readAsString("/json/2.0.0/model/info/info.json"),
-                Info::class.java
-        )
-    }
+    override fun baseObjectJson() = "/json/2.0.0/model/asyncapi.json"
 
-    private fun buildChannels(): Map<String, ChannelItem> {
-        val channelItem = objectMapper.readValue(
-                ClasspathUtils.readAsString("/json/2.0.0/model/channel/channel.json"),
-                object: TypeReference<ChannelItem>() {}
-        )
+    override fun extendedObjectJson() = "/json/2.0.0/model/asyncapi - extended.json"
 
-        return mapOf(Pair("channelName", channelItem))
-    }
+    override fun wronglyExtendedObjectJson() = "/json/2.0.0/model/asyncapi - wrongly extended.json"
 
-    private fun buildServers(): Map<String, Server> {
-        val server = objectMapper.readValue(
-                ClasspathUtils.readAsString("/json/2.0.0/model/server/server.json"),
-                Server::class.java
-        )
-
-        return mapOf(Pair("stage", server))
-    }
-
-    private fun buildTags(): List<Tag> {
-        val tag = objectMapper.readValue(
-                ClasspathUtils.readAsString("/json/2.0.0/model/tag.json"),
-                Tag::class.java
-        )
-
-        return listOf(tag)
-    }
-
-    private fun buildExternalDocs(): ExternalDocumentation {
-        return objectMapper.readValue(
-                ClasspathUtils.readAsString("/json/2.0.0/model/externalDocumentation.json"),
-                ExternalDocumentation::class.java
-        )
-    }
-
-    private fun buildComponents(): Components {
-        return objectMapper.readValue(
-                ClasspathUtils.readAsString("/json/2.0.0/model/components/components.json"),
-                object: TypeReference<Components>() {}
-        )
-    }
-
-    private fun buildAsyncAPI(): AsyncAPI {
+    override fun build(): AsyncAPI {
         return AsyncAPI(
                 "2.0.0",
                 "https://www.asyncapi.com",
                 "application/json",
-                buildInfo(),
-                buildServers(),
-                buildChannels(),
-                buildComponents(),
-                buildTags(),
-                buildExternalDocs()
-        )
-    }
-
-    @Test
-    @DisplayName("Compare hand crafted model with parsed json")
-    fun compareModelWithParsedJson() {
-        val model = ClasspathUtils.readAsString("/json/2.0.0/model/asyncapi.json")
-
-        Assertions.assertEquals(
-                objectMapper.readValue(model, AsyncAPI::class.java),
-                buildAsyncAPI()
+                InfoTest().build(),
+                mapOf(
+                        Pair("stage", ServerTest().build())
+                ),
+                mapOf(
+                        Pair("messages", ChannelItemTest().build())
+                ),
+                ComponentsTest().build(),
+                listOf(
+                        TagTest().build()
+                ),
+                ExternalDocumentationTest().build()
         )
     }
 
