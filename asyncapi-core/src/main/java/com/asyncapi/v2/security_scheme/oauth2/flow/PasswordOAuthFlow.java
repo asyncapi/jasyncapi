91a314255642138b0e960dc1ccea1e5669543804
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-18 01:16:42 +0400

    feat: PasswordOAuthFlow default values for @NotNull fields

--- asyncapi-core/src/main/java/com/asyncapi/v2/security_scheme/oauth2/flow/PasswordOAuthFlow.java
+++ asyncapi-core/src/main/java/com/asyncapi/v2/security_scheme/oauth2/flow/PasswordOAuthFlow.java
@@ -27,7 +27,7 @@ public class PasswordOAuthFlow extends OAuthFlow {
      * The token URL to be used for this flow. This MUST be in the form of a URL.
      */
     @NotNull
-    private String tokenUrl;
+    private String tokenUrl = "";
 
     @Builder(builderMethodName = "passwordOAuthFlowBuilder")
     public PasswordOAuthFlow(@Nullable String refreshUrl,
