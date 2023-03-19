8bb7764a5f1ea126d7ff3fa3c08f0088d7324148
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-19 16:32:27 +0400

    refactor: GooglePubSubMessageBinding
    - Validation API
    - Jackson annotations
    - Properties extracted to own classes

---
+++ asyncapi-core/src/main/java/com/asyncapi/v2/binding/message/googlepubsub/GooglePubSubMessageSchemaDefinitionType.java
@@ -0,0 +1,23 @@
+package com.asyncapi.v2.binding.message.googlepubsub;
+
+import com.fasterxml.jackson.annotation.JsonClassDescription;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Describes Google Cloud Pub/Sub message schema definition type.
+ *
+ * @version 0.1.0
+ * @see <a href="https://github.com/asyncapi/bindings/tree/master/googlepubsub#message-binding-object">Google Cloud Pub/Sub message binding</a>
+ * @see <a href="https://cloud.google.com/pubsub/docs/schemas#types-schema">Types of schemas</a>
+ * @author Pavel Bodiachevskii
+ */
+@JsonClassDescription("Describes Google Cloud Pub/Sub message schema definition type.")
+public enum GooglePubSubMessageSchemaDefinitionType {
+
+    @JsonProperty("avro")
+    AVRO,
+
+    @JsonProperty("protobuf")
+    PROTOBUF
+
+}
