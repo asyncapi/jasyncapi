18a75f04a90c3809b372a5ce9eefef42cac26ab1
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-21 00:27:16 +0400

    refactor: PulsarChannelBinding
    - Validation API
    - Jackson annotations
    - properties extracted to own classes

--- asyncapi-core/src/main/java/com/asyncapi/v2/binding/channel/pulsar/PulsarChannelBinding.java
+++ asyncapi-core/src/main/java/com/asyncapi/v2/binding/channel/pulsar/PulsarChannelBinding.java
@@ -1,7 +1,9 @@
 package com.asyncapi.v2.binding.channel.pulsar;
 
 import com.asyncapi.v2.binding.channel.ChannelBinding;
+import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,24 +26,39 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@JsonClassDescription("Describes Pulsar channel binding.")
 public class PulsarChannelBinding extends ChannelBinding {
 
     /**
      * The namespace the channel is associated with.
      */
     @NotNull
-    private String namespace;
+    @Builder.Default
+    @javax.validation.constraints.NotNull
+    @JsonProperty("namespace")
+    @JsonPropertyDescription("The namespace the channel is associated with.")
+    private String namespace = "";
 
     /**
      * Persistence of the topic in Pulsar. It MUST be either persistent or non-persistent.
      */
     @NotNull
-    private String persistence;
+    @Builder.Default
+    @javax.validation.constraints.NotNull
+    @JsonProperty(value = "persistence", defaultValue = "persistent")
+    @JsonPropertyDescription("Persistence of the topic in Pulsar. It MUST be either persistent or non-persistent.")
+    private PulsarChannelPersistence persistence = PulsarChannelPersistence.PERSISTENT;
 
     /**
      * Topic compaction threshold given in Megabytes.
      */
     @Nullable
+    @javax.validation.constraints.Min(
+            value = 0,
+            message = "Topic compaction threshold must be greater or equals to 0."
+    )
+    @JsonProperty("compaction")
+    @JsonPropertyDescription("Topic compaction threshold given in Megabytes.")
     private Integer compaction;
 
     /**
@@ -49,24 +66,31 @@ public class PulsarChannelBinding extends ChannelBinding {
      */
     @Nullable
     @JsonProperty("geo-replication")
+    @JsonPropertyDescription("A list of clusters the topic is replicated to.")
     private List<String> geoReplication;
 
     /**
      * Topic retention policy.
      */
     @Nullable
-    private RetentionDefinition retention;
+    @JsonProperty("retention")
+    @JsonPropertyDescription("Topic retention policy.")
+    private PulsarChannelRetentionDefinition retention;
 
     /**
      * Message time-to-live in seconds.
      */
     @Nullable
+    @JsonProperty("ttl")
+    @JsonPropertyDescription("Message time-to-live in seconds.")
     private Integer ttl;
 
     /**
      * Message deduplication. When true, it ensures that each message produced on Pulsar topics is persisted to disk only once.
      */
     @Nullable
+    @JsonProperty("deduplication")
+    @JsonPropertyDescription("Message deduplication. When true, it ensures that each message produced on Pulsar topics is persisted to disk only once.")
     private Boolean deduplication;
 
     /**
@@ -74,32 +98,8 @@ public class PulsarChannelBinding extends ChannelBinding {
      */
     @Nullable
     @Builder.Default
+    @JsonProperty("bindingVersion")
+    @JsonPropertyDescription("The version of this binding.")
     private String bindingVersion = "0.1.0";
-
-    /**
-     * The Retention Definition Object is used to describe the Pulsar <a href="https://pulsar.apache.org/docs/cookbooks-retention-expiry/">Retention</a> policy.
-     */
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @EqualsAndHashCode
-    public static class RetentionDefinition {
-
-        /**
-         * Time given in Minutes.
-         */
-        @Nullable
-        @Builder.Default
-        private Integer time = 0;
-
-        /**
-         * Size given in MegaBytes.
-         */
-        @Nullable
-        @Builder.Default
-        private Integer size = 0;
-
-    }
 
 }
