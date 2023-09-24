ec8919437a32c849dd00c0fd795407e86e717d24
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-09-24 20:05:32 +0400

    feat(3.0.0): Tag

---
+++ asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/jackson/model/ExternalDocumentationDeserializer.java
@@ -0,0 +1,23 @@
+package com.asyncapi.v3._0_0.jackson.model;
+
+import com.asyncapi.v3.Reference;
+import com.asyncapi.v3._0_0.model.ExternalDocumentation;
+import com.asyncapi.v3.jackson.ReferenceOrObjectDeserializer;
+
+/**
+ * Deserializes external documentation.
+ *
+ * @author Pavel Bodiachevskii
+ */
+public class ExternalDocumentationDeserializer extends ReferenceOrObjectDeserializer<ExternalDocumentation> {
+
+    @Override
+    public Class<ExternalDocumentation> objectTypeClass() {
+        return ExternalDocumentation.class;
+    }
+
+    public Class<?> referenceClass() {
+        return Reference.class;
+    }
+
+}
