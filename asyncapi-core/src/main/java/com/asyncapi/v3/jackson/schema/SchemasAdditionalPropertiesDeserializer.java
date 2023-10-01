d904d037c8cc195de046e8fcfbcc7b145bb20240
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-10-02 03:07:00 +0400

    fix(3.0.0): SchemasAdditionalPropertiesDeserializer fix schema version

--- asyncapi-core/src/main/java/com/asyncapi/v3/jackson/schema/SchemasAdditionalPropertiesDeserializer.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/jackson/schema/SchemasAdditionalPropertiesDeserializer.java
@@ -1,6 +1,6 @@
 package com.asyncapi.v3.jackson.schema;
 
-import com.asyncapi.v2.schema.Schema;
+import com.asyncapi.v3.schema.Schema;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
