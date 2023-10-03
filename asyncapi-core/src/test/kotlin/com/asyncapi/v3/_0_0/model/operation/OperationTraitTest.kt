5445d18301fdb8e67198f9ebc85a3f13ef11180b
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-10-04 02:24:07 +0400

    feat(3.0.0): OperationTrait

---
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v3/_0_0/model/operation/OperationTraitTest.kt
@@ -0,0 +1,134 @@
+package com.asyncapi.v3._0_0.model.operation
+
+import com.asyncapi.v3.binding.operation.amqp.AMQPOperationBindingTest
+import com.asyncapi.v3.binding.operation.http.HTTPOperationBindingTest
+import com.asyncapi.v3.binding.operation.kafka.KafkaOperationBindingTest
+import com.asyncapi.v3.binding.operation.mqtt.MQTTOperationBindingTest
+import com.asyncapi.v3.binding.operation.nats.NATSOperationBindingTest
+import com.asyncapi.v3.binding.operation.solace.SolaceOperationBindingTest
+import com.asyncapi.v3.Reference
+import com.asyncapi.v3.SerDeTest
+import com.asyncapi.v3._0_0.model.ExternalDocumentation
+import com.asyncapi.v3._0_0.model.Tag
+import com.asyncapi.v3._0_0.model.operation.reply.OperationReplyTest
+import com.asyncapi.v3.security_scheme.ApiKeySecuritySchemeTest
+
+/**
+ * @version 3.0.0
+ * @author Pavel Bodiachevskii
+ */
+class OperationTraitTest: SerDeTest<OperationTrait>() {
+
+    override fun objectClass() = OperationTrait::class.java
+
+    override fun baseObjectJson() = "/json/v3/3.0.0/model/operation/operationTrait.json"
+
+    override fun extendedObjectJson() = "/json/v3/3.0.0/model/operation/operationTrait - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/v3/3.0.0/model/operation/operationTrait - wrongly extended.json"
+
+    override fun build(): OperationTrait {
+        return OperationTrait.builder()
+                .title("Send message operation")
+                .summary("Send message")
+                .description("Send message to remote server")
+                .security(listOf(
+                        ApiKeySecuritySchemeTest().build(),
+                        Reference("#/components/security/plain")
+                ))
+                .tags(listOf(
+                        Tag("messages", "operations with messages", ExternalDocumentation(
+                                "Messages validation rules", "messages/validation-rules"
+                        )),
+                        Reference("#/components/tags/tag")
+                ))
+                .externalDocs(ExternalDocumentation("Messages validation rules", "messages/validation-rules"))
+                .bindings(mapOf(
+                        Pair("amqp", AMQPOperationBindingTest().build()),
+                        Pair("amqp1", Reference("#/components/operationBindings/amqp1")),
+                        Pair("anypointmq", Reference("#/components/operationBindings/anypointmq")),
+                        Pair("googlepubsub", Reference("#/components/operationBindings/googlepubsub")),
+                        Pair("http", HTTPOperationBindingTest().build()),
+                        Pair("ibmmq", Reference("#/components/operationBindings/ibmmq")),
+                        Pair("jms", Reference("#/components/operationBindings/jms")),
+                        Pair("kafka", KafkaOperationBindingTest().build()),
+                        Pair("mercure", Reference("#/components/operationBindings/mercure")),
+                        Pair("mqtt", MQTTOperationBindingTest().build()),
+                        Pair("mqtt5", Reference("#/components/operationBindings/mqtt5")),
+                        Pair("nats", NATSOperationBindingTest().build()),
+                        Pair("pulsar", Reference("#/components/operationBindings/pulsar")),
+                        Pair("redis", Reference("#/components/operationBindings/redis")),
+                        Pair("sns", Reference("#/components/operationBindings/sns")),
+                        Pair("solace", SolaceOperationBindingTest().build()),
+                        Pair("sqs", Reference("#/components/operationBindings/sqs")),
+                        Pair("stomp", Reference("#/components/operationBindings/stomp")),
+                        Pair("ws", Reference("#/components/operationBindings/ws"))
+                ))
+                .messages(listOf(
+                        Reference("#/components/messages/message 1"),
+                        Reference("#/components/messages/message 2"),
+                        Reference("#/components/messages/message 3"),
+                ))
+                .reply(OperationReplyTest().build())
+                .build()
+    }
+
+}
+
+class OperationTraitTestWithReference: SerDeTest<OperationTrait>() {
+
+    override fun objectClass() = OperationTrait::class.java
+
+    override fun baseObjectJson() = "/json/v3/3.0.0/model/operation/operationTrait with reference.json"
+
+    override fun extendedObjectJson() = "/json/v3/3.0.0/model/operation/operationTrait with reference - extended.json"
+
+    override fun wronglyExtendedObjectJson() = "/json/v3/3.0.0/model/operation/operationTrait with reference - wrongly extended.json"
+
+    override fun build(): OperationTrait {
+        return OperationTrait.builder()
+                .title("Send message operation")
+                .summary("Send message")
+                .description("Send message to remote server")
+                .security(listOf(
+                        ApiKeySecuritySchemeTest().build(),
+                        Reference("#/components/security/plain")
+                ))
+                .tags(listOf(
+                        Tag("messages", "operations with messages", ExternalDocumentation(
+                                "Messages validation rules", "messages/validation-rules"
+                        )),
+                        Reference("#/components/tags/tag")
+                ))
+                .externalDocs(Reference("#/components/externalDocs/external-doc"))
+                .bindings(mapOf(
+                        Pair("amqp", AMQPOperationBindingTest().build()),
+                        Pair("amqp1", Reference("#/components/operationBindings/amqp1")),
+                        Pair("anypointmq", Reference("#/components/operationBindings/anypointmq")),
+                        Pair("googlepubsub", Reference("#/components/operationBindings/googlepubsub")),
+                        Pair("http", HTTPOperationBindingTest().build()),
+                        Pair("ibmmq", Reference("#/components/operationBindings/ibmmq")),
+                        Pair("jms", Reference("#/components/operationBindings/jms")),
+                        Pair("kafka", KafkaOperationBindingTest().build()),
+                        Pair("mercure", Reference("#/components/operationBindings/mercure")),
+                        Pair("mqtt", MQTTOperationBindingTest().build()),
+                        Pair("mqtt5", Reference("#/components/operationBindings/mqtt5")),
+                        Pair("nats", NATSOperationBindingTest().build()),
+                        Pair("pulsar", Reference("#/components/operationBindings/pulsar")),
+                        Pair("redis", Reference("#/components/operationBindings/redis")),
+                        Pair("sns", Reference("#/components/operationBindings/sns")),
+                        Pair("solace", SolaceOperationBindingTest().build()),
+                        Pair("sqs", Reference("#/components/operationBindings/sqs")),
+                        Pair("stomp", Reference("#/components/operationBindings/stomp")),
+                        Pair("ws", Reference("#/components/operationBindings/ws"))
+                ))
+                .messages(listOf(
+                        Reference("#/components/messages/message 1"),
+                        Reference("#/components/messages/message 2"),
+                        Reference("#/components/messages/message 3"),
+                ))
+                .reply(Reference("#/components/replies/reply"))
+                .build()
+    }
+
+}
