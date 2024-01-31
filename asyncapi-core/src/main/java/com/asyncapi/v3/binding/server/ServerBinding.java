3fbd254e750b77bd3ac3f8f8ebcb1ebba4a646c6
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-01 02:37:03 +0400

    docs(3.0.0): update ServerBinding JavaDocs

--- asyncapi-core/src/main/java/com/asyncapi/v3/binding/server/ServerBinding.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/binding/server/ServerBinding.java
@@ -4,10 +4,14 @@ import com.asyncapi.v3.ExtendableObject;
 import lombok.EqualsAndHashCode;
 
 /**
- * Describes AsyncAPI server binding.
+ * Describes protocol-specific definition for a server.
+ * <p>
+ * This object MAY be extended with {@link ExtendableObject}.
  *
- * @version 3.0.0
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#specificationExtensions">Specification Extensions</a>
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#serverBindingsObject">Server Binding</a>
  * @author Pavel Bodiachevskii
+ * @version 3.0.0
  */
 @EqualsAndHashCode(callSuper = true)
 public class ServerBinding extends ExtendableObject {
