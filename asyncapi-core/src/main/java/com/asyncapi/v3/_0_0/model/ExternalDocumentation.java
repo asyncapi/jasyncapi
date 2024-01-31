ef873e15a476c888b219fd58a236f59f36c127fb
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-01 02:14:22 +0400

    docs(3.0.0): update ExternalDocumentation JavaDocs

--- asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/ExternalDocumentation.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/ExternalDocumentation.java
@@ -8,9 +8,9 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Allows referencing an external resource for extended documentation.
  *
- * @version 3.0.0
- * @see <a href="https://www.asyncapi.com/docs/specifications/2.0.0/#externalDocumentationObject">ExternalDocumentation</a>
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#externalDocumentationObject">ExternalDocumentation</a>
  * @author Pavel Bodiachevskii
+ * @version 3.0.0
  */
 @Data
 @Builder
@@ -20,13 +20,13 @@ import org.jetbrains.annotations.Nullable;
 public class ExternalDocumentation extends ExtendableObject {
 
     /**
-     * A short description of the target documentation. CommonMark syntax can be used for rich text representation.
+     * A short description of the target documentation. <a href="https://spec.commonmark.org/">CommonMark syntax</a> can be used for rich text representation.
      */
     @Nullable
     private String description;
 
     /**
-     * Required.
+     * <b>Required</b>.
      * <p>
      * The URL for the target documentation. Value MUST be in the format of a URL.
      */
