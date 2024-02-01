b99586aec7ef0df4b39efe6babdfd39a01070b27
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-01 23:16:19 +0400

    docs(3.0.0): update ChannelBinding JavaDocs

--- asyncapi-core/src/main/java/com/asyncapi/v3/binding/channel/ChannelBinding.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/binding/channel/ChannelBinding.java
@@ -4,9 +4,14 @@ import com.asyncapi.v3.ExtendableObject;
 import lombok.EqualsAndHashCode;
 
 /**
- * Describes AsyncAPI channel binding.
+ * Describes protocol-specific definition for a channel.
+ * <p>
+ * This object MAY be extended with {@link ExtendableObject}.
  *
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#specificationExtensions">Specification Extensions</a>
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#channelBindingsObject">Channel Binding</a>
  * @author Pavel Bodiachevskii
+ * @version 3.0.0
  */
 @EqualsAndHashCode(callSuper = true)
 public class ChannelBinding extends ExtendableObject {
