451d60879e5a3666092997b244efd0b252220cc1
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-02 01:48:03 +0400

    docs(3.0.0): update MessageExample JavaDocs

--- asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/channel/message/MessageExample.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/channel/message/MessageExample.java
@@ -7,7 +7,11 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 
 /**
- * Message Example Object represents an example of a {@link Message} Object and MUST contain either headers and/or payload fields.
+ * Message Example Object represents an example of a {@link Message} Object and MUST contain either {@link Message#getHeaders()} and/or {@link Message#getPayload()} fields.
+ *
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#messageExampleObject">Message Example</a>
+ * @author Pavel Bodiachevskii
+ * @version 3.0.0
  */
 @Data
 @Builder
