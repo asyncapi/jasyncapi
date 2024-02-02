81cbfafc71fa0ed38ea120a097eecf2917c982f3
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-02 22:20:18 +0400

    docs(3.0.0): update Operation JavaDocs

--- asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/operation/Operation.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/operation/Operation.java
@@ -24,9 +24,9 @@ import java.util.Map;
 /**
  * Describes a specific operation.
  *
- * @version 3.0.0
- * @see <a href="https://www.asyncapi.com/docs/reference/specification/v2.6.0#operationObject">Operation</a>
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#operationObject">Operation</a>
  * @author Pavel Bodiachevskii
+ * @version 3.0.0
  */
 @Data
 @Builder
@@ -36,19 +36,26 @@ import java.util.Map;
 public class Operation extends ExtendableObject {
 
     /**
-     * Required.
+     * <b>Required</b>.
      * <p>
-     * Use send when it's expected that the application will send a message to the given channel, and receive when
-     * the application should expect receiving messages from the given channel.
+     * Use send when it's expected that the application will send a message to the given {@link com.asyncapi.v3._0_0.model.channel.Channel},
+     * and receive when the application should expect receiving messages from the given {@link com.asyncapi.v3._0_0.model.channel.Channel}.
      */
     @NotNull
     private OperationAction action;
 
     /**
-     * Required.
+     * <b>Required</b>.
      * <p>
      * A $ref pointer to the definition of the channel in which this operation is performed.
+     * <p>
+     * If the operation is located in the root {@link com.asyncapi.v3._0_0.model.AsyncAPI#getOperations()}, it MUST point to a channel
+     * definition located in the {@link com.asyncapi.v3._0_0.model.AsyncAPI#getChannels()}, and MUST NOT point to a channel definition located
+     * in the {@link com.asyncapi.v3._0_0.model.AsyncAPI#getComponents()} or anywhere else.
+     * <p>
+     * If the operation is located in the {@link com.asyncapi.v3._0_0.model.AsyncAPI#getComponents()}, it MAY point to a Channel Object in any location.
      * Please note the channel property value MUST be a Reference Object and, therefore, MUST NOT contain a Channel Object.
+     * <p>
      * However, it is RECOMMENDED that parsers (or other software) dereference this property for a better development experience.
      */
     @NotNull
@@ -67,20 +74,22 @@ public class Operation extends ExtendableObject {
     private String summary;
 
     /**
-     * A verbose explanation of the operation. CommonMark syntax can be used for rich text representation.
+     * A verbose explanation of the operation. <a href="http://spec.commonmark.org/">CommonMark syntax</a> can be used for rich text representation.
      */
     @Nullable
     private String description;
 
     /**
      * A declaration of which security schemes are associated with this operation.
-     * Only one of the security scheme objects MUST be satisfied to authorize an operation.
+     * <p>
+     * Only one of the {@link SecurityScheme} objects MUST be satisfied to authorize an operation.
+     * <p>
      * In cases where Server Security also applies, it MUST also be satisfied.
      * <p>
      * MUST BE:
      * <ul>
-     *     <li>{@link Reference}</li>
      *     <li>{@link SecurityScheme}</li>
+     *     <li>{@link Reference}</li>
      * </ul>
      */
     @Nullable
@@ -92,8 +101,8 @@ public class Operation extends ExtendableObject {
      * <p>
      * MUST BE:
      * <ul>
-     *     <li>{@link Reference}</li>
      *     <li>{@link Tag}</li>
+     *     <li>{@link Reference}</li>
      * </ul>
      */
     @Nullable
@@ -105,8 +114,8 @@ public class Operation extends ExtendableObject {
      * <p>
      * MUST BE:
      * <ul>
-     *     <li>{@link Reference}</li>
      *     <li>{@link ExternalDocumentation}</li>
+     *     <li>{@link Reference}</li>
      * </ul>
      */
     @Nullable
@@ -118,8 +127,8 @@ public class Operation extends ExtendableObject {
      * <p>
      * MUST BE:
      * <ul>
-     *     <li>{@link Reference}</li>
      *     <li>{@link OperationBinding}</li>
+     *     <li>{@link Reference}</li>
      * </ul>
      */
     @Nullable
@@ -128,25 +137,32 @@ public class Operation extends ExtendableObject {
 
     /**
      * A list of traits to apply to the operation object. Traits MUST be merged using traits merge mechanism.
-     * The resulting object MUST be a valid Operation Object.
+     * <p>
+     * The resulting object MUST be a valid {@link Operation}.
      * <p>
      * MUST BE:
      * <ul>
-     *     <li>{@link Reference}</li>
      *     <li>{@link OperationTrait}</li>
+     *     <li>{@link Reference}</li>
      * </ul>
+     *
+     * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#traits-merge-mechanism">Traits Merge Mechanism</a>
      */
     @Nullable
     @JsonDeserialize(using = OperationTraitsDeserializer.class)
     private List<Object> traits;
 
     /**
-     * A list of $ref pointers pointing to the supported Message Objects that can be processed by this operation.
-     * It MUST contain a subset of the messages defined in the channel referenced in this operation.
-     * Every message processed by this operation MUST be valid against one, and only one, of the message objects
-     * referenced in this list. Please note the messages property value MUST be a list of Reference Objects and,
-     * therefore, MUST NOT contain Message Objects. However, it is RECOMMENDED that parsers (or other software)
-     * dereference this property for a better development experience.
+     * A list of $ref pointers pointing to the supported {@link com.asyncapi.v3._0_0.model.channel.message.Message} that can be processed by this operation.
+     * <p>
+     * It MUST contain a subset of the messages defined in the channel referenced in this operation - {@link Operation#getChannel()},
+     * and MUST NOT point to a subset of message definitions located in the {@link com.asyncapi.v3._0_0.model.component.Components#getMessages()} in the {@link com.asyncapi.v3._0_0.model.AsyncAPI#getComponents()} or anywhere else.
+     * <p>
+     * <b>Every message processed by this operation MUST be valid against one, and only one, of the message objects referenced in this list</b>.
+     * <p>
+     * Please note the messages property value MUST be a list of Reference Objects and, therefore, MUST NOT contain Message Objects.
+     * <p>
+     * However, it is RECOMMENDED that parsers (or other software) dereference this property for a better development experience.
      */
     @Nullable
     private List<Reference> messages;
@@ -156,8 +172,8 @@ public class Operation extends ExtendableObject {
      * <p>
      * MUST BE:
      * <ul>
-     *     <li>{@link Reference}</li>
      *     <li>{@link OperationReply}</li>
+     *     <li>{@link Reference}</li>
      * </ul>
      */
     @Nullable
