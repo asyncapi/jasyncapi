9933391f79374b78ca723436dbfe377dd5af00d1
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-02 22:31:46 +0400

    docs(3.0.0): update OperationBinding JavaDocs

--- asyncapi-core/src/main/java/com/asyncapi/v3/binding/operation/OperationBinding.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/binding/operation/OperationBinding.java
@@ -4,9 +4,14 @@ import com.asyncapi.v3.ExtendableObject;
 import lombok.EqualsAndHashCode;
 
 /**
- * Describes AsyncAPI operation binding.
+ * Describes protocol-specific definition for an operation.
+ * <p>
+ * This object MAY be extended with {@link ExtendableObject}.
  *
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#specificationExtensions">Specification Extensions</a>
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#operationBindingsObject">Operation Binding</a>
  * @author Pavel Bodiachevskii
+ * @version 3.0.0
  */
 @EqualsAndHashCode(callSuper = true)
 public class OperationBinding extends ExtendableObject {
