f8b235a93c0e8715ea11c6f985d33c85c3cf9d56
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-02 22:25:04 +0400

    docs(3.0.0): update OperationAction JavaDocs

--- asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/operation/OperationAction.java
+++ asyncapi-core/src/main/java/com/asyncapi/v3/_0_0/model/operation/OperationAction.java
@@ -2,11 +2,24 @@ package com.asyncapi.v3._0_0.model.operation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Describes a specific operation action.
+ *
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#operationObject">Operation</a>
+ * @author Pavel Bodiachevskii
+ * @version 3.0.0
+ */
 public enum OperationAction {
 
+    /**
+     * Use send when it's expected that the application will send a message to the given {@link com.asyncapi.v3._0_0.model.channel.Channel}.
+     */
     @JsonProperty("send")
     SEND,
 
+    /**
+     * Use receive when the application should expect receiving messages from the given {@link com.asyncapi.v3._0_0.model.channel.Channel}.
+     */
     @JsonProperty("receive")
     RECEIVE
 
