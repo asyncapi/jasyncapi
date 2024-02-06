a2b9838e6acb78fe3749d0fc4200819a7e60f431
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-02-06 23:08:26 +0400

    test(3.0.0): check correctness of realisation by reading AsyncAPI example - Simple AsyncAPI example

---
+++ asyncapi-core/src/test/kotlin/com/asyncapi/examples/v3/_0_0/SimpleAsyncAPI.kt
@@ -0,0 +1,83 @@
+package com.asyncapi.examples.v3._0_0
+
+import com.asyncapi.v3.Reference
+import com.asyncapi.v3._0_0.model.channel.Channel
+import com.asyncapi.v3._0_0.model.channel.message.Message
+import com.asyncapi.v3._0_0.model.component.Components
+import com.asyncapi.v3._0_0.model.info.Info
+import com.asyncapi.v3._0_0.model.operation.Operation
+import com.asyncapi.v3._0_0.model.operation.OperationAction
+import com.asyncapi.v3.schema.Schema
+
+class SimpleAsyncAPI: AbstractExampleValidationTest() {
+
+    override fun specificationLocation(): String = "/examples/v3.0.0/simple-asyncapi.yml"
+
+    override fun expectedInfo(): Info {
+        return Info.builder()
+                .title("Account Service")
+                .version("1.0.0")
+                .description("This service is in charge of processing user signups")
+                .build()
+    }
+
+    override fun expectedServers(): Map<String, Any> = emptyMap()
+
+    override fun expectedChannels(): Map<String, Any> {
+        return mapOf(
+                Pair("userSignedup",
+                        Channel.builder()
+                                .address("user/signedup")
+                                .messages(mapOf(
+                                        Pair("UserSignedUp", Reference("#/components/messages/UserSignedUp"))
+                                ))
+                                .build()
+                )
+        )
+    }
+
+    override fun expectedOperations(): Map<String, Any> {
+        return mapOf(
+                Pair("sendUserSignedup",
+                        Operation.builder()
+                                .action(OperationAction.SEND)
+                                .channel(Reference("#/channels/userSignedup"))
+                                .messages(listOf(
+                                        Reference("#/channels/userSignedup/messages/UserSignedUp")
+                                ))
+                                .build()
+                )
+        )
+    }
+
+    override fun expectedComponents(): Components {
+        return Components.builder()
+                .messages(mapOf(
+                        Pair("UserSignedUp",
+                                Message.builder()
+                                        .payload(Schema.builder()
+                                                .type("object")
+                                                .properties(mapOf(
+                                                        Pair("displayName",
+                                                                Schema.builder()
+                                                                        .type("string")
+                                                                        .description("Name of the user")
+                                                                        .build()
+                                                        ),
+                                                        Pair("email",
+                                                                Schema.builder()
+                                                                        .type("string")
+                                                                        .format("email")
+                                                                        .description("Email of the user")
+                                                                        .build()
+                                                        )
+                                                ))
+                                                .build()
+                                        )
+                                        .build()
+                        )
+                ))
+                .build()
+    }
+
+}
