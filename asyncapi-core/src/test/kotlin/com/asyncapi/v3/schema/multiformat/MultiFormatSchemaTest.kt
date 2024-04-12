1967dba7019dea9bf90931914c21422d3bb085fa
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-04-13 01:23:08 +0400

    test(schemaFormat): refactor Json Schema tests
    https://github.com/asyncapi/jasyncapi/issues/185

--- asyncapi-core/src/test/kotlin/com/asyncapi/v3/schema/multiformat/MultiFormatSchemaTest.kt
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v3/schema/multiformat/MultiFormatSchemaTest.kt
@@ -1,42 +1,15 @@
 package com.asyncapi.v3.schema.multiformat
 
-import com.asyncapi.v3.ClasspathUtils
-import com.asyncapi.v3.schema.SchemaProvider
-import com.asyncapi.v3.schema.json.*
 import com.asyncapi.v3.schema.multiformat.asyncapi.*
+import com.asyncapi.v3.schema.multiformat.json.JsonFormatSchemaTest
 import com.asyncapi.v3.schema.multiformat.openapi.OpenAPIFormatSchemaV3_0_0Test
 import com.asyncapi.v3.schema.multiformat.openapi.OpenAPIFormatSchemaV3_0_1Test
 import com.asyncapi.v3.schema.multiformat.openapi.OpenAPIFormatSchemaV3_0_2Test
 import com.asyncapi.v3.schema.multiformat.openapi.OpenAPIFormatSchemaV3_0_3Test
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.extension.ExtensionContext
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.ArgumentsProvider
-import org.junit.jupiter.params.provider.ArgumentsSource
-import java.util.stream.Stream
 
 class MultiFormatSchemaTest {
-
-    private val objectMapper: ObjectMapper = ObjectMapper(YAMLFactory())
-        .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-        .findAndRegisterModules()
-
-    fun <Schema> compareSchemas(
-        schemaToCompareWith: String,
-        schemaClass: Class<*>,
-        schemaToCheck: Schema
-    ) {
-        val jsonSchemaString = ClasspathUtils.readAsString(schemaToCompareWith)
-        val jsonSchema = objectMapper.readValue(jsonSchemaString, schemaClass)
-
-        Assertions.assertEquals(jsonSchema, schemaToCheck)
-    }
 
     @Nested
     inner class AsyncAPISchema {
@@ -90,17 +63,9 @@ class MultiFormatSchemaTest {
     @Nested
     inner class JsonSchema {
 
-        @ArgumentsSource(JsonSchemasJsonFormat::class)
-        @ParameterizedTest(name = "Read: {0}")
-        fun json(schemaToCompareWith: String, schemaProvider: SchemaProvider) {
-            compareSchemas(schemaToCompareWith, JsonFormatSchema::class.java, schemaProvider.jsonFormatSchemaJson())
-        }
-
-        @ArgumentsSource(JsonSchemasYamlFormat::class)
-        @ParameterizedTest(name = "Read: {0}")
-        fun yaml(schemaToCompareWith: String, schemaProvider: SchemaProvider) {
-            compareSchemas(schemaToCompareWith, JsonFormatSchema::class.java, schemaProvider.jsonFormatSchemaYaml())
-        }
+        @Nested
+        @DisplayName("Draft-07")
+        inner class Draft07: JsonFormatSchemaTest()
 
     }
 
@@ -122,38 +87,6 @@ class MultiFormatSchemaTest {
         @Nested
         @DisplayName("3.0.3")
         inner class V3_0_3: OpenAPIFormatSchemaV3_0_3Test()
-
-    }
-
-    class JsonSchemasJsonFormat: ArgumentsProvider {
-
-        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
-            return Stream.of(
-                Arguments.of("/json/v3/schema/multiformat/json/arrays.schema.json", ArraysSchemaTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/complex-object.schema.json", ComplexObjectTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/conditional-validation-if-else.schema.json", ConditionalValidationIfElse()),
-                Arguments.of("/json/v3/schema/multiformat/json/draft-07-core-schema-meta-schema.json", Draft07CoreSchemaMetaSchemaTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/enumerated-values.schema.json", EnumeratedValuesTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/person.schema.json", PersonTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/regex-pattern.schema.json", RegexPatternTest())
-            )
-        }
-
-    }
-
-    class JsonSchemasYamlFormat: ArgumentsProvider {
-
-        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
-            return Stream.of(
-                Arguments.of("/json/v3/schema/multiformat/json/arrays.schema.yaml", ArraysSchemaTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/complex-object.schema.yaml", ComplexObjectTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/conditional-validation-if-else.schema.yaml", ConditionalValidationIfElse()),
-                Arguments.of("/json/v3/schema/multiformat/json/draft-07-core-schema-meta-schema.yaml", Draft07CoreSchemaMetaSchemaTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/enumerated-values.schema.yaml", EnumeratedValuesTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/person.schema.yaml", PersonTest()),
-                Arguments.of("/json/v3/schema/multiformat/json/regex-pattern.schema.yaml", RegexPatternTest())
-            )
-        }
 
     }
 
---
+++ asyncapi-core/src/test/kotlin/com/asyncapi/v3/schema/multiformat/json/JsonFormatSchemaTest.kt
@@ -0,0 +1,126 @@
+package com.asyncapi.v3.schema.multiformat.json
+
+import com.asyncapi.v3.ClasspathUtils
+import com.asyncapi.v3.schema.json.*
+import com.asyncapi.v3.schema.multiformat.JsonFormatSchema
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+abstract class JsonFormatSchemaTest {
+
+    private val objectMapper: ObjectMapper = ObjectMapper(YAMLFactory())
+        .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        .findAndRegisterModules()
+
+    fun compareSchemas(
+        jsonFormatSchemaToCompareWithFilePath: String,
+        schemaToCheck: JsonFormatSchema
+    ) {
+        val schemaAsJson = ClasspathUtils.readAsString(jsonFormatSchemaToCompareWithFilePath)
+        val schema = objectMapper.readValue(schemaAsJson, JsonFormatSchema::class.java)
+
+        Assertions.assertEquals(schema, schemaToCheck)
+    }
+
+    @ArgumentsSource(Json::class)
+    @ParameterizedTest(name = "Read: {0}")
+    fun parseJson(
+        jsonFormatSchemaToCompareWithFilePath: String,
+        jsonFormatSchema: JsonFormatSchema
+    ) {
+        compareSchemas(jsonFormatSchemaToCompareWithFilePath, jsonFormatSchema)
+
+    }
+
+    @ArgumentsSource(Yaml::class)
+    @ParameterizedTest(name = "Read: {0}")
+    fun parseYaml(
+        jsonFormatSchemaToCompareWithFilePath: String,
+        jsonFormatSchema: JsonFormatSchema
+    ) {
+        compareSchemas(jsonFormatSchemaToCompareWithFilePath, jsonFormatSchema)
+    }
+
+    class Json: ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+json/arrays.schema.json",
+                    JsonFormatSchema(ArraysSchemaTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+json/complex-object.schema.json",
+                    JsonFormatSchema(ComplexObjectTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+json/conditional-validation-if-else.schema.json",
+                    JsonFormatSchema(ConditionalValidationIfElse().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+json/draft-07-core-schema-meta-schema.json",
+                    JsonFormatSchema(Draft07CoreSchemaMetaSchemaTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+json/enumerated-values.schema.json",
+                    JsonFormatSchema(EnumeratedValuesTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+json/person.schema.json",
+                    JsonFormatSchema(PersonTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+json/regex-pattern.schema.json",
+                    JsonFormatSchema(RegexPatternTest().jsonSchema())
+                )
+            )
+        }
+
+    }
+
+    class Yaml: ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+yaml/arrays.schema.yaml",
+                    JsonFormatSchema("application/schema+yaml;version=draft-07", ArraysSchemaTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+yaml/complex-object.schema.yaml",
+                    JsonFormatSchema("application/schema+yaml;version=draft-07", ComplexObjectTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+yaml/conditional-validation-if-else.schema.yaml",
+                    JsonFormatSchema("application/schema+yaml;version=draft-07", ConditionalValidationIfElse().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+yaml/draft-07-core-schema-meta-schema.yaml",
+                    JsonFormatSchema("application/schema+yaml;version=draft-07", Draft07CoreSchemaMetaSchemaTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+yaml/enumerated-values.schema.yaml",
+                    JsonFormatSchema("application/schema+yaml;version=draft-07", EnumeratedValuesTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+yaml/person.schema.yaml",
+                    JsonFormatSchema("application/schema+yaml;version=draft-07", PersonTest().jsonSchema())
+                ),
+                Arguments.of(
+                    "/json/v3/schema/multiformat/json/schema+yaml/regex-pattern.schema.yaml",
+                    JsonFormatSchema("application/schema+yaml;version=draft-07", RegexPatternTest().jsonSchema())
+                )
+            )
+        }
+
+    }
+
+}
