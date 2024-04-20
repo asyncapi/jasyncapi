d31e556e34b73b85184b59dbebba836fe10aa0df
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-04-21 00:52:38 +0400

    feat(bindings): move bindings to root package
    https://github.com/asyncapi/jasyncapi/issues/184

---
+++ asyncapi-core/src/main/java/com/asyncapi/bindings/MessageBinding.java
@@ -0,0 +1,13 @@
+package com.asyncapi.bindings;
+
+import com.asyncapi.v3.ExtendableObject;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Describes AsyncAPI message binding.
+ *
+ * @author Pavel Bodiachevskii
+ */
+@EqualsAndHashCode(callSuper = true)
+public class MessageBinding extends ExtendableObject {
+}
---
+++ asyncapi-core/src/main/java/com/asyncapi/bindings/OperationBinding.java
@@ -0,0 +1,18 @@
+package com.asyncapi.bindings;
+
+import com.asyncapi.v3.ExtendableObject;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Describes protocol-specific definition for an operation.
+ * <p>
+ * This object MAY be extended with {@link ExtendableObject}.
+ *
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#specificationExtensions">Specification Extensions</a>
+ * @see <a href="https://www.asyncapi.com/docs/reference/specification/v3.0.0#operationBindingsObject">Operation Binding</a>
+ * @author Pavel Bodiachevskii
+ * @version 3.0.0
+ */
+@EqualsAndHashCode(callSuper = true)
+public class OperationBinding extends ExtendableObject {
+}
